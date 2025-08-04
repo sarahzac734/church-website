85b0a25495fabed7dba5363eb65fcfc17fe0af4b
sarahzac734 <sarahzachariah13@gmail.com>
2025-08-04 15:17:49 -0400

    trying to fix vercel 8

--- src/app/components/NewsList.tsx
+++ src/app/components/NewsList.tsx
@@ -17,11 +17,11 @@ export default async function NewsList () {
    const {news} = await getNews()
 
    const items = news.map((n:NewsProp) => (
-            <li key={n._id}>
+            <li key={1}>
             <div  className="p-4 border border-slate-300 my-3 mx-4 flex justify-between gap-4 items-start">
                 <div>
-                    <h2 className="font-bold text-xl">{n.title}</h2>
-                    <div>{n.description}</div>
+                    <h2 className="font-bold text-xl">hi</h2>
+                    <div>hi</div>
                 </div>
             </div>
             </li>
