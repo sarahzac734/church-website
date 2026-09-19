5b3b6b854f79990650183c3ec9d0a9a1ef1e5caa
sarahzac734 <sarahzachariah13@gmail.com>
2026-09-19 03:00:54 -0400

    email.

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,21 +1,21 @@
-import { NextResponse } from 'next/server'
-import { Resend } from 'resend'
-import Email from '@/emails/message'
+// import { NextResponse } from 'next/server'
+// import { Resend } from 'resend'
+// import Email from '@/emails/message'
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 
-export async function POST(request: Request) {
-    const { firstName, lastName, email, comment, reason} = await request.json();
+// export async function POST(request: Request) {
+//     const { firstName, lastName, email, comment, reason} = await request.json();
 
-    await resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'churchcalendar242@gmail.com',
-        subject: reason,
-        react: Email({firstName, lastName, email, comment})
-      });
+//     await resend.emails.send({
+//         from: 'onboarding@resend.dev',
+//         to: 'churchcalendar242@gmail.com',
+//         subject: reason,
+//         react: Email({firstName, lastName, email, comment})
+//       });
 
-    return NextResponse.json({
-        status:'ok'
-    })
-}
+//     return NextResponse.json({
+//         status:'ok'
+//     })
+// }
